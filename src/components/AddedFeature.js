6f864185eb5d6da20a07edc6558197ce1eb8903d
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-06 17:08:02 -0500

    connected addFeature and removeFeature to buttons

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeFeature } from "../actions";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={props.removeFeature(props.feature)}>
+      <button
+        className="button"
+        onClick={() => props.removeFeature(props.feature)}
+      >
         X
       </button>
       {props.feature.name}
@@ -12,4 +17,4 @@ const AddedFeature = props => {
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,24 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import { removeFeature } from "../actions";
 import AddedFeature from "./AddedFeature";
 
 const AddedFeatures = props => {
-  const removeFeature = feature => {
-    props.removeFeature(feature);
-  };
-
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature
-              key={item.id}
-              feature={item}
-              removeFeature={removeFeature}
-            />
+            <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
@@ -34,4 +25,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { removeFeature })(AddedFeatures);
+export default connect(mapStateToProps)(AddedFeatures);
